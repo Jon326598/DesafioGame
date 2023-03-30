08aea5de67672f928f0aeb4b826f9387ebdac1b7
Jonathan Carvalho <jjc_oliveira@hotmail.com>
2023-03-30 00:31:32 -0300

    finalizado parte do pulo e game-over

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 const mario = document.querySelectorAll('.mario')[0];
-        const jump = () => {
-            mario.classList.add("jump");
-            setTimeout(()=>{
-                mario.classList.remove('jump');
-            },500);
-        }
-        document.addEventListener('keydown', jump);
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add("jump");
+
+    setTimeout(()=>{
+    mario.classList.remove('jump');
+     },500);
+}       
+
+const loop = setInterval(() =>{
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+    if(pipePosition <= 80 && pipePosition > 0 && marioPosition < 80){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "./images/game-over.png";
+       
+        mario.style.marginLeft = '5px'
+
+    }
+
+}, 10);
+
+    document.addEventListener('keydown', jump);
